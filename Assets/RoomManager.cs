e47489dbbb06d8c975ba50021c443e391cb64a68
William Bradshaw <williambradshaw604@gmail.com>
2020-03-02 15:52:12 -0800

    loading room backgrounds with room manager

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomManager : MonoBehaviour
+{
+
+    public GameManager gameState;
+    public Image background;
+
+    public Sprite[] roomBackgrounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // set current roomBG
+        int currentRoomID = (int) gameState.currentRoom;
+        background.sprite = roomBackgrounds[currentRoomID];
+
+    }
+}
