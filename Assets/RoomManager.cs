48132b7df4b516c52e692b4abba32e4aade534a1
William Bradshaw <williambradshaw604@gmail.com>
2020-03-02 17:28:42 -0800

    example of loading canvas with buttons dynamically into scene

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -11,10 +11,20 @@ public class RoomManager : MonoBehaviour
 
     public Sprite[] roomBackgrounds;
 
+    public AudioClip[] gameBGM;
+
+    public GameObject[] roomCanvasLayouts;
+
+    public Fungus.Flowchart[] roomDialogues;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        // generate room canvas
+        int currentRoomID = (int)gameState.currentRoom;
+        Instantiate(roomCanvasLayouts[currentRoomID]);
+
     }
 
     // Update is called once per frame
