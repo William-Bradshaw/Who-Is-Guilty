2ddaedcfa59bba321f71e053fd0ab68904cc3020
William Bradshaw <williambradshaw604@gmail.com>
2020-03-31 10:04:04 -0700

    adding Lily as NPC

---
+++ Assets/NPC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class NPC : MonoBehaviour
+{
+
+    public string dialogueName;
+
+    private Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject flowchartObject = GameObject.Find("Flowchart");
+        flowchart = flowchartObject.GetComponent<Flowchart>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void speakToNPC()
+    {
+        Debug.Log("talking to NPC!");
+
+        flowchart.ExecuteBlock(dialogueName);
+
+    }
+}
